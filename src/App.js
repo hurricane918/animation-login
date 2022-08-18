11f66aa5226d23c14622dfc515f9dc5035225aad
hurricane918 <hurricaneninja918@gmail.com>
2022-08-19 00:08:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import LoginForm from "./Components/LoginFrom";
+import NavBar from "./Components/NavBar";
 
 function App() {
+  const [isShowLogin, setIsShowLogin] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsShowLogin((isShowLogin) => !isShowLogin)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar handleLoginClick={handleLoginClick} />
+      <LoginForm isShowLogin={isShowLogin} />
     </div>
   );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NavBar({handleLoginClick}) {
+    const handleClick = () => {
+        handleLoginClick();
+    }
+
+    return (
+        <div className="navbar">
+            <div>
+                <span onClick={handleClick} className="loginicon">Sign In</span>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
